9270dddee246fa32ba6f86f0d152647cbaddef00
Mengting Fang <mtfang@stanford.edu>
2018-01-06 18:43:08 -0800

    Create model_settings.py

---
+++ model_settings.py
@@ -0,0 +1,13 @@
+img_height = 100
+img_width = 100
+min_edge = 2
+max_edge = 5
+p_size = 2
+min_blobs_train = 1
+max_blobs_train = 9
+min_blobs_test = 1
+max_blobs_test = 9
+
+learning_rate = 1e-2
+glimpses = 10 #10 run prep, glimpses+1
+batch_size = 1
