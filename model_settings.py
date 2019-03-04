ba9dbf230c049fc29c10004a1b683ffd37acdb66
Mengting Fang <mtfang@stanford.edu>
2019-03-04 11:21:53 -0500

    Update model_settings.py

--- model_settings.py
+++ model_settings.py
@@ -4,10 +4,10 @@ min_edge = 2
 max_edge = 5
 p_size = 2
 min_blobs_train = 1
-max_blobs_train = 9
+max_blobs_train = 15
 min_blobs_test = 1
 max_blobs_test = 9
 
 learning_rate = 1e-2
-glimpses = 10 #10 run prep, glimpses+1
-batch_size = 1
+glimpses = max_blobs_train - min_blobs_train + 2
+batch_size = 100
